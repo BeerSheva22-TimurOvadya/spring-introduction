7624e2d2ca6b626ec3aba2de6fdadef3a8e29379
Ovadya33 <timurgts2710@gmail.com>
2023-07-27 12:08:36 +0300

    classWork

---
+++ src/main/java/telran/spring/SpringInroductionApplication.java
@@ -0,0 +1,19 @@
+package telran.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@SpringBootApplication
+public class SpringInroductionApplication {
+
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(SpringInroductionApplication.class, args);
+	}
+	
+}
