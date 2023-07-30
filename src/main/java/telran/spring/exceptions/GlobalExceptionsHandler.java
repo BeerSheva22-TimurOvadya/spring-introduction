159304fbab3f60b2e6e23e0652aef9987f3e5ed1
Ovadya33 <timurgts2710@gmail.com>
2023-07-30 11:32:03 +0300

    classwork

--- src/main/java/telran/spring/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/telran/spring/exceptions/GlobalExceptionsHandler.java
@@ -1,8 +1,13 @@
 package telran.spring.exceptions;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionsHandler {
-
+	@ExceptionHandler(NotFoundException.class)
+	ResponseEntity<String> notFoundHandler() {
+		
+	}
 }
